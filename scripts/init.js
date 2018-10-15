6b5bf0c969b17d4c0bb8211a5c373f027617c898
ahidh <ahidh.hi@gmail.com>
2018-10-15 20:45:02 +0530

    Update scripts/init.js

--- scripts/init.js
+++ scripts/init.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
 	var currentPage = document.location.href.substr(0); //get current page url
 	$("#nav-main ul li a").each(function(num, val){
 	    if($(val).attr('href') == currentPage){
-	        $(val).addClass('active');
+	        $(val).addClass('current');
 	    } else{
-	        $(val).removeClass('active');
+	        $(val).removeClass('current');
 	    }
 	});
 });
