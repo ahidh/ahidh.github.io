75cff4ce402193721b750c257bfc34b405f2e5af
ahidh <ahidh.hi@gmail.com>
2018-10-15 21:27:31 +0530

    Adding .current css

--- scripts/init.js
+++ scripts/init.js
@@ -1,10 +1,11 @@
-$(document).ready(function() {
-	var currentPage = document.location.href.substr(0); //get current page url
-	$("#nav-main ul li a").each(function(num, val){
-	    if($(val).attr('href') == currentPage){
-	        $(val).addClass('current');
-	    } else{
-	        $(val).removeClass('current');
-	    }
-	});
+$(function(){
+    var url = window.location.pathname, 
+        urlRegExp = new RegExp(url.replace(/\/$/,'') + "$"); // create regexp to match current url pathname and remove trailing slash if present as it could collide with the link in navigation in case trailing slash wasn't present there
+        // now grab every link from the navigation
+        $('#nav-main a').each(function(){
+            // and test its normalized href against the url pathname regexp
+            if(urlRegExp.test(this.href.replace(/\/$/,''))){
+                $(this).addClass('current');
+            }
+        });
 });
